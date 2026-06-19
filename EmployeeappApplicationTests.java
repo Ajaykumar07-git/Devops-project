968d1645cd0fd801365027cca9dce3e642a53b9c
AJay kumar vemula <ajaykumarvemula07@gmail.com>
2026-06-19 16:41:05 +0530

    Initial Spring Boot Application

---
+++ EmployeeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ajay.employeeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
